884affe3ae5b5cf1a009ff568f3eb38f487272d4
nopenoway0 <benjamin.gottheil@sjsu.edu>
2017-08-19 15:02:37 -0700

    added GetProcessName function. Lists names of inectable running processes

--- Injector/Header/MiscFunc.h
+++ Injector/Header/MiscFunc.h
@@ -9,9 +9,11 @@
 #include "fstream"
 #include <vector>
 #include <atlconv.h>
+#include <Psapi.h>
 
 namespace MiscFunc{
 	bool Inject(const char* library_name, const char* processName);
 	DWORD FindProcessId(const std::wstring& processName);
+	void GetProcessNames(std::vector<std::wstring>* list_of_names);
 }
 #endif
