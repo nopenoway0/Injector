3f0670ec884637d47e981e51c38cd44a9f4360b7
nopenoway0 <benjamin.gottheil@sjsu.edu>
2017-08-19 15:00:34 -0700

    added event binding for refresh removed icon header

--- Injector/Source/Injector.cpp
+++ Injector/Source/Injector.cpp
@@ -3,7 +3,6 @@
 
 #include <wx/wxprec.h>
 #include "Menu.h"
-#include <wx/icon.h>
 #ifndef WX_PRECOMP
 	#include <wx/wx.h>
 #endif
@@ -24,4 +23,5 @@ BEGIN_EVENT_TABLE(Menu, wxFrame)
 	EVT_BUTTON(wxID_INJECT,  Menu::Inject)
 	EVT_BUTTON(wxID_SAVE_CONFIG, Menu::SaveConfig)
 	EVT_BUTTON(wxID_LOAD_CONFIG, Menu::LoadConfig)
+	EVT_BUTTON(wxID_REFRESH_PROC, Menu::Refresh)
 END_EVENT_TABLE()
